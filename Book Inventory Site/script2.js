0613cd6bea803ace5924e9cf7b8b0bdd30809d66
samer-alsalib <142041961+samer-alsalib@users.noreply.github.com>
2023-12-03 14:14:45 +0300

    Delete Book Inventory Site directory

--- Book Inventory Site/script2.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-    let cartItemsUlCheckout = document.querySelector("#cart-items-ul");
-    cartItemsUlCheckout.style.border = "5px solid black";
-
-    cartItemsUlCheckout.innerHTML = sessionStorage.getItem("ulItems");
-};
